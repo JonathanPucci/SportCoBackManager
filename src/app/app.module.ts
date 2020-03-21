134bf88e8aad6c4bb8e75d52dfc8e8abe3902f3c
JonathanPucci <jonathanpucci06@gmail.com>
2020-03-21 23:34:06 +0100

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { BsModalModule } from "ng2-bs3-modal";
 
 import { AppComponent } from "./app.component";
 import { DbmanagerComponent } from "./dbmanager/dbmanager.component";
@@ -15,7 +14,6 @@ import { EntityService } from "./dbmanager/entity/entity-service.service";
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    BsModalModule
   ],
   providers: [EntityService],
   bootstrap: [AppComponent]
